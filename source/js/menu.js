7ec02e0f2cbd1dfe1fcdf2f29b6a3198863e14bb
Чечулин Владислав Александрович <Chechulin.V.Al@sberbank.ru>
2019-07-29 13:19:48 +0300

    поправил контейнер-центровщик на десктопном разрешении и отрефакторил скрипт открытия меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -7,12 +7,9 @@
   menu.classList.add('main-nav--close');
 
   toggle.addEventListener('click', function() {
-    if (toggle.classList.contains('header__toggle--open')) {
-      toggle.classList.remove('header__toggle--open');
-      menu.classList.add('main-nav--close');
-    } else {
-      toggle.classList.add('header__toggle--open');
-      menu.classList.remove('main-nav--close');
-    }
+    var isToggleOpen = toggle.classList.contains('header__toggle--open')
+
+    toggle.classList.toggle('header__toggle--open', !isToggleOpen);
+    menu.classList.toggle('main-nav--close', isToggleOpen);
   })
 })()
